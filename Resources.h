ef42a63baee4c677e4e96cb906d5467595a7ff95
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-21 08:33:33 +0900

    feat: implement loadResources
    in debugging

--- Resources.h
+++ Resources.h
@@ -11,39 +11,54 @@
 #define RESOURCE_FISH_LEN 1
 #define RESOURCE_BACKGROUND_LEN 1
 
-char* RESOURCE_CAT[RESOURCE_CAT_LEN];
-char* RESOURCE_FISH[RESOURCE_FISH_LEN];
-char* RESOURCE_BACKGROUND[RESOURCE_BACKGROUND_LEN];
+char** RESOURCE_CAT[RESOURCE_CAT_LEN];
+char** RESOURCE_FISH[RESOURCE_FISH_LEN];
+char** RESOURCE_BACKGROUND[RESOURCE_BACKGROUND_LEN];
+
+char **loadResources(int resourceLength, char* formatString) {
+  int **resourceList = malloc(resourceLength * sizeof(char *));
+  for (int idx = 1; idx <= resourceLength; idx++) {
+    char filename[100];
+    sprintf(filename, formatString, idx);
+    int len = strlen(filename) + 1;
+    (*resourceList)[idx - 1] = (char*)malloc(sizeof(char) * len);
+    strcpy((*resourceList)[idx - 1], filename);
+  }
+}
 
 inline void updateResources() {
   // TODO: Abstraction needed!!!
 
   // export cat resources to array automatically
-  for (int idx = 1; idx <= RESOURCE_CAT_LEN; idx++) {
-    char filename[100];
-    sprintf(filename, "resources/cat/cat-p%d.bmp", idx);
-    int len = strlen(filename) + 1;
-    RESOURCE_CAT[idx - 1] = (char*)malloc(sizeof(char) * len);
-    strcpy(RESOURCE_CAT[idx - 1], filename);
-  }
+  // for (int idx = 1; idx <= RESOURCE_CAT_LEN; idx++) {
+  //   char filename[100];
+  //   sprintf(filename, "resources/cat/cat-p%d.bmp", idx);
+  //   int len = strlen(filename) + 1;
+  //   RESOURCE_CAT[idx - 1] = (char*)malloc(sizeof(char) * len);
+  //   strcpy(RESOURCE_CAT[idx - 1], filename);
+  // }
+  // loadResources(&RESOURCE_CAT, RESOURCE_CAT_LEN, "resources/cat/cat-p%d.bmp");
 
   // export fish resources to array automatically
-  for (int idx = 1; idx <= RESOURCE_FISH_LEN; idx++) {
-    char filename[100];
-    sprintf(filename, "resources/fish/fish-lv%d.bmp", idx);
-    int len = strlen(filename) + 1;
-    RESOURCE_FISH[idx - 1] = (char*)malloc(sizeof(char) * len);
-    strcpy(RESOURCE_FISH[idx - 1], filename);
-  }
+  // for (int idx = 1; idx <= RESOURCE_FISH_LEN; idx++) {
+  //   char filename[100];
+  //   sprintf(filename, "resources/fish/fish-lv%d.bmp", idx);
+  //   int len = strlen(filename) + 1;
+  //   RESOURCE_FISH[idx - 1] = (char*)malloc(sizeof(char) * len);
+  //   strcpy(RESOURCE_FISH[idx - 1], filename);
+  // }
 
-  // export background stages
-  for (int idx = 1; idx <= RESOURCE_BACKGROUND_LEN; idx++) {
-    char filename[100];
-    sprintf(filename, "resources/background/background-st%d.bmp", idx);
-    int len = strlen(filename) + 1;
-    RESOURCE_BACKGROUND[idx - 1] = (char*)malloc(sizeof(char) * len);
-    strcpy(RESOURCE_BACKGROUND[idx - 1], filename);
-  }
+  // // export background stages
+  // for (int idx = 1; idx <= RESOURCE_BACKGROUND_LEN; idx++) {
+  //   char filename[100];
+  //   sprintf(filename, "resources/background/background-st%d.bmp", idx);
+  //   int len = strlen(filename) + 1;
+  //   RESOURCE_BACKGROUND[idx - 1] = (char*)malloc(sizeof(char) * len);
+  //   strcpy(RESOURCE_BACKGROUND[idx - 1], filename);
+  // }
+  **RESOURCE_CAT = loadResources(RESOURCE_CAT_LEN, "resources/cat/cat-p%d.bmp");
+  // char **RESOURCE_FISH[RESOURCE_FISH_LEN];
+  // char **RESOURCE_BACKGROUND[RESOURCE_BACKGROUND_LEN];
 }
 
 #endif
