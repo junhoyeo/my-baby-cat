dbbed961c8e49ec71d6ea41acf5c80e1fc694fc0
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-23 13:13:36 +0900

    feat: Item 타입 추가

--- Objects/Item/ItemIntf.h
+++ Objects/Item/ItemIntf.h
@@ -3,6 +3,11 @@
 #ifndef _OBJ_ITEM_INTF_
 #define _OBJ_ITEM_INTF_
 
+#define ITEM_TYPE_HEART 0 // 생명 더 주는 아이템
+#define ITEM_TYPE_FASTER 1 // 속도 증가 아이템
+#define ITEM_TYPE_SLOWER 2 // 속도 감소 아이템
+#define ITEM_TYPE_GRACE 3 // 무적 아이템
+
 typedef struct _Item {
   int x, y;
   int width, height;
