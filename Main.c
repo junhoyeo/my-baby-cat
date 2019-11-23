a8e1bea57fc95542f9db96a09cf024afac146994
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-23 16:45:09 +0900

    feat: 물고기 생성 위치 변경을 통해 더 자연스럽게

--- Main.c
+++ Main.c
@@ -27,28 +27,26 @@ int _animateFishSegments_shown = 0;
 // bool _animateFishSegments_done = false;
 
 void animateFishSegments(int fishLength) {
-  for (int repeat = 0;; repeat++) {
+  while (1) {
     for (int idx = 0; idx < fishLength; idx++) {
-      if (repeat > 2 * idx) {
-        if ((*fishSegmentPointer)[idx]->x <= 200) { // 플레이어와 만남
-          // TODO: 점프, 슬라이드 시 플레이어가 해당 물고기를 먹을 수 있는지 확인
-          Image *image = (*fishSegmentPointer)[idx]->image;
-          if (image->isShown) {
-            // 살아 있는 물고기면 안 보이게 처리
-            image->isShown = false;
-            SCORE.update(&SCORE, 100);
-            // _animateFishSegments_finished++;
-          }
-        } else { // 이동 가능
-          // printf("%d\n", (*fishSegmentPointer)[idx]->x);
-          Sleep(SPEED.delay / 4);
-          (*fishSegmentPointer)[idx]->move((*fishSegmentPointer)[idx], SPEED.delay / 10);
-          if ((*fishSegmentPointer)[idx]->x <= 1400) {
-            _animateFishSegments_shown++;
-          }
+      if ((*fishSegmentPointer)[idx]->x <= 200) { // 플레이어와 만남
+        // TODO: 점프, 슬라이드 시 플레이어가 해당 물고기를 먹을 수 있는지 확인
+        Image *image = (*fishSegmentPointer)[idx]->image;
+        if (image->isShown) {
+          // 살아 있는 물고기면 안 보이게 처리
+          image->isShown = false;
+          SCORE.update(&SCORE, 100);
+          // _animateFishSegments_finished++;
+        }
+      } else { // 이동 가능
+        // printf("%d\n", (*fishSegmentPointer)[idx]->x);
+        (*fishSegmentPointer)[idx]->move((*fishSegmentPointer)[idx], SPEED.delay / 10);
+        if ((*fishSegmentPointer)[idx]->x <= 1400) {
+          _animateFishSegments_shown++;
         }
       }
     }
+    Sleep(SPEED.delay / 15);
   }
 }
 
@@ -56,22 +54,12 @@ Obstacle *(*ObstacleSegmentPointer)[] = NULL;
 int _animateObstacleSegments_shown = 0;
 
 void animateObstacleSegments(int obstacleLength) {
-  for (int repeat = 0;; repeat++) {
+  while (1) {
     for (int idx = 0; idx < obstacleLength; idx++) {
-      if (repeat > 10 * idx) {
-        //   // if ((*ObstacleSegmentPointer)[idx]->x <= 200) { // 플레이어와 만남
-        //   // } else { // 이동 가능
-        //   //   (*ObstacleSegmentPointer)[idx]->move((*ObstacleSegmentPointer)[idx], 10);
-        //   //   // if ((*ObstacleSegmentPointer)[idx]->x <= 1400) {
-        //   //   //   _animateFishSegments_shown++;
-        //   //   // }
-        //   // }
-        // }
-        Sleep(SPEED.delay / 5);
-        (*ObstacleSegmentPointer)[idx]->move((*ObstacleSegmentPointer)[idx], SPEED.delay / 10);
-        if ((*ObstacleSegmentPointer)[idx]->x <= 1500) {
-          _animateObstacleSegments_shown++;
-        }
+      Sleep(SPEED.delay / 10);
+      (*ObstacleSegmentPointer)[idx]->move((*ObstacleSegmentPointer)[idx], SPEED.delay / 20);
+      if ((*ObstacleSegmentPointer)[idx]->x <= 1500) {
+        _animateObstacleSegments_shown++;
       }
     }
   }
@@ -81,14 +69,12 @@ Item *(*ItemSegmentPointer)[] = NULL;
 int _animateItemSegments_shown = 0;
 
 void animateItemSegments(int itemLength) {
-  for (int repeat = 0;; repeat++) {
+  while (1) {
     for (int idx = 0; idx < itemLength; idx++) {
-      if (repeat > 10 * idx) {
-        Sleep(SPEED.delay / 5);
-        (*ItemSegmentPointer)[idx]->move((*ItemSegmentPointer)[idx], SPEED.delay / 10);
-        if ((*ItemSegmentPointer)[idx]->x <= 1500) {
-          _animateItemSegments_shown++;
-        }
+      Sleep(SPEED.delay / 5);
+      (*ItemSegmentPointer)[idx]->move((*ItemSegmentPointer)[idx], SPEED.delay / 20);
+      if ((*ItemSegmentPointer)[idx]->x <= 1500) {
+        _animateItemSegments_shown++;
       }
     }
   }
@@ -157,7 +143,7 @@ int main() {
         images[imageLayer.imageCount] = (Image) { .fileName = RESOURCE_FISH[0], .x = 1900, .y = 600, .scale = 1, .isShown = true };
 
         fishSegments[i] = malloc(sizeof(Fish));
-        *fishSegments[i] = createFish();
+        *fishSegments[i] = createFish(1900 + i * 200);
         fishSegments[i]->image = &images[imageLayer.imageCount];
         fishSegments[i]->imageLayer = &imageLayer;
         fishSegments[i]->init(fishSegments[i]);
