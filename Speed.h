fe05c804fb06db15c5013ad5a886232c9b1fc910
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-23 19:56:51 +0900

    feat: 속도 증감 효과 메소드 추가

--- Speed.h
+++ Speed.h
@@ -7,6 +7,8 @@ typedef struct _Speed {
   int delay;
 
   void (*init)(struct _Speed*);
+  void (*increase)(struct _Speed*);
+  void (*decrease)(struct _Speed*);
   void (*addBackgroundThread)(struct _Speed*, void (*method)(struct _Speed*));
 } Speed;
 
@@ -14,17 +16,27 @@ void _Speed_init(Speed* self) {
   for (int speed = self->delay; speed > 50; speed-=20) {
     Sleep(800);
     self->delay = speed;
-    gotoxy(0,0);
-    printf("[%d]\n", self->delay);
   }
 }
 
+// 서서히 딜레이 값을 25 줄입니다. 3초 유지 후 바로 25를 올립니다.
 void _Speed_increase(Speed*self) {
-  
+  for (int i = 0; i < 5; i++) {
+    Sleep(50);
+    self->delay += 5;
+  }
+  Sleep(3000);
+  self->delay += 25;
 }
 
+// 서서히 딜레이 값을 25 늘립니다. 3초 유지 후 바로 25를 줄입니다.
 void _Speed_decrease(Speed*self) {
-  
+  for (int i = 0; i < 5; i++) {
+    Sleep(50);
+    self->delay += 5;
+  }
+  Sleep(3000);
+  self->delay -= 25;
 }
 
 void _Speed_addBackgroundThread(Speed* self, int (*method)(Speed*)) {
@@ -41,6 +53,8 @@ Speed createSpeed() {
   return (Speed) {
     .delay = 150,
     .init = _Speed_init,
+    .increase = _Speed_increase,
+    .decrease = _Speed_decrease,
     .addBackgroundThread = _Speed_addBackgroundThread,
   };
 }
